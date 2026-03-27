c28af6cd4992078b34981a2cbbc318e9f852a931
Dewey Ou <oushihao97@gmail.com>
2026-03-27 11:14:25 +0800

    build(styles): restore publish subpath exports

--- packages/styles/vite.config.ts
+++ packages/styles/vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite-plus';
 import { defineConfig as definePackConfig } from 'vite-plus/pack';
 
+const stylesPublicExports = {
+  '.': {
+    types: './dist/index.d.mts',
+    import: './dist/index.mjs',
+    default: './dist/index.mjs',
+  },
+  './color.css': './dist/css/color.css',
+  './theme.css': './dist/css/theme.css',
+  './theme-light.css': './dist/css/theme-light.css',
+  './theme-dark.css': './dist/css/theme-dark.css',
+  './reset.css': './dist/css/reset.css',
+  './base.css': './dist/css/base.css',
+  './less/bridge.less': './dist/less/bridge.less',
+  './less/mixins.less': './dist/less/mixins.less',
+};
+
 const defaultPackConfig = definePackConfig({
-  exports: true,
+  exports: {
+    customExports: stylesPublicExports,
+  },
 });
 
 export default defineConfig({
