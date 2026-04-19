4201ceaf7328ab3fdcaefa31964703f3306d9fca
Dewey Ou <oushihao97@gmail.com>
2026-04-19 18:37:00 +0800

    feat(react-icons): add named icon exports wrapping tabler icons

---
+++ packages/react-icons/src/icons/index.ts
@@ -0,0 +1,65 @@
+import {
+  IconAlertCircle,
+  IconAlertTriangle,
+  IconArrowLeft,
+  IconArrowRight,
+  IconBell,
+  IconCheck,
+  IconChevronDown,
+  IconChevronLeft,
+  IconChevronRight,
+  IconChevronUp,
+  IconCopy,
+  IconDownload,
+  IconEdit,
+  IconExternalLink,
+  IconEye,
+  IconEyeOff,
+  IconFilter,
+  IconHome,
+  IconInfoCircle,
+  IconLoader2,
+  IconMenu2,
+  IconMinus,
+  IconPlus,
+  IconRefresh,
+  IconSearch,
+  IconSettings,
+  IconTrash,
+  IconUpload,
+  IconUser,
+  IconX,
+} from '@tabler/icons-react';
+
+import { createTablerIcon } from '../icon-wrapper';
+
+export const AlertCircleIcon = createTablerIcon(IconAlertCircle);
+export const AlertTriangleIcon = createTablerIcon(IconAlertTriangle);
+export const ArrowLeftIcon = createTablerIcon(IconArrowLeft);
+export const ArrowRightIcon = createTablerIcon(IconArrowRight);
+export const BellIcon = createTablerIcon(IconBell);
+export const CheckIcon = createTablerIcon(IconCheck);
+export const ChevronDownIcon = createTablerIcon(IconChevronDown);
+export const ChevronLeftIcon = createTablerIcon(IconChevronLeft);
+export const ChevronRightIcon = createTablerIcon(IconChevronRight);
+export const ChevronUpIcon = createTablerIcon(IconChevronUp);
+export const CopyIcon = createTablerIcon(IconCopy);
+export const DownloadIcon = createTablerIcon(IconDownload);
+export const EditIcon = createTablerIcon(IconEdit);
+export const ExternalLinkIcon = createTablerIcon(IconExternalLink);
+export const EyeIcon = createTablerIcon(IconEye);
+export const EyeOffIcon = createTablerIcon(IconEyeOff);
+export const FilterIcon = createTablerIcon(IconFilter);
+export const HomeIcon = createTablerIcon(IconHome);
+export const InfoIcon = createTablerIcon(IconInfoCircle);
+export const Loader2Icon = createTablerIcon(IconLoader2);
+export const Menu2Icon = createTablerIcon(IconMenu2);
+export const MinusIcon = createTablerIcon(IconMinus);
+export const PlusIcon = createTablerIcon(IconPlus);
+export const RefreshIcon = createTablerIcon(IconRefresh);
+export const SearchIcon = createTablerIcon(IconSearch);
+export const SettingsIcon = createTablerIcon(IconSettings);
+export const TrashIcon = createTablerIcon(IconTrash);
+export const UploadIcon = createTablerIcon(IconUpload);
+export const UserIcon = createTablerIcon(IconUser);
+export const XIcon = createTablerIcon(IconX);
